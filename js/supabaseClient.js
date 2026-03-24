477a4a8b2b556f5463d157158bdaffbd63870add
Sreejith S <113572634+sreejithadra96@users.noreply.github.com>
2026-03-24 16:53:46 +0530

    Initialize Supabase client with URL and key

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://qzpesrgomdzopribdcmp.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6cGVzcmdvbWR6b3ByaWJkY21wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNTAzMzUsImV4cCI6MjA3NzcyNjMzNX0.27INrKOblIko8OhEOxbQE0_jrWRosziUKV-TbSYkmKY"
 
+const supabaseClient = supabase.createClient(
+   SUPABASE_URL,
+   SUPABASE_KEY
+);
